1b62ca5a58d8b388d4bc650c6ed6a5e66dd85b90
mjf <miaojuanfeng@qq.com>
2019-11-30 16:58:32 +0800

    asd

--- src/main/java/gov/gz/hydrology/controller/IframeController.java
+++ src/main/java/gov/gz/hydrology/controller/IframeController.java
@@ -265,11 +265,27 @@ public class IframeController {
             plan.setQRss0(p.getQRss0());
             plan.setQRg0(p.getQRg0());
 
-            List<Map> stations = stationService.selectChildStationByStcd(plan.getStcd());
+//            List<Map> stations = stationService.selectChildStationByStcd(plan.getStcd());
+//            List<String> stcdId = new ArrayList<>();
+//            for (int i = 0; i < stations.size(); i++) {
+//                stcdId.add(String.valueOf(stations.get(i).get("stcd")));
+//            }
+
             List<String> stcdId = new ArrayList<>();
-            for (int i = 0; i < stations.size(); i++) {
-                stcdId.add(String.valueOf(stations.get(i).get("stcd")));
-            }
+            stcdId.add("62302350");
+            stcdId.add("62321000");
+            stcdId.add("62321010");
+            stcdId.add("62321020");
+            stcdId.add("62321030");
+            stcdId.add("62321045");
+            stcdId.add("62321050");
+            stcdId.add("62321055");
+            stcdId.add("62321065");
+            stcdId.add("62321070");
+            stcdId.add("62321085");
+            stcdId.add("62321100");
+            stcdId.add("62323620");
+
             List<Rainfall> rainfalls = rainfallService.selectRainfallRange(stcdId, forecastTime, affectTime);
             List<BigDecimal> rainfallArr = new ArrayList<>();
             List<String> timeArr = new ArrayList<>();
